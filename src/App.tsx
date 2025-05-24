3d0a6d9c02d77d3286b2798b669b73e1df13a22f
Rapinder <rapinder@flowell.co>
2025-05-24 16:05:48 +0530

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import "./App.css";
 import { Box, Container, Typography } from "@mui/material";
-import { useHabitStore } from "./stores/store";
 import AddHabbitForm from "./components/AddHabbitForm";
 import HabitList from "./components/HabitList";
 
 function App() {
-  // const store = useHabitStore();
-  // console.log("store", store);
   return (
     <Container>
       <Box>
--- src/components/HabitList.tsx
+++ src/components/HabitList.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Habit, useHabitStore } from "../stores/store";
 import {
   Box,
